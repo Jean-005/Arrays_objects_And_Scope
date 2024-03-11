920f1dbaecb890636c6f1d367e2c554818d8ed39
Jean <jean-ata@hotmail.co.uk>
2024-03-11 19:00:21 +0000

    completed the scope task

--- code/arrays.js
+++ code/arrays.js
@@ -2,23 +2,24 @@
  * In these tasks you will need to manipulate the array below in order to answer the questions. Some answers will require you to store the result in a variable, others will need you to inspect the whole array.
  */
 
-stations = ["Dundee", "Edinburgh", "North Berwick", "Newcastle", "York", "Leeds", "Doncaster", "Peterborough"];
+// let stations = ["Dundee", "Edinburgh", "North Berwick", "Newcastle", "York", "Leeds", "Doncaster", "Peterborough"];
 
 /**
  * Q1.
  * The East Coast Main Line doesn't stop at Peterborough! Add London Kings Cross to the end of the array to complete the line.
  */
 
-// console.log(stations);
-
+let stations = ["Dundee", "Edinburgh", "North Berwick", "Newcastle", "York", "Leeds", "Doncaster", "Peterborough"];
+stations.push("London Kings Cross")
+console.log(stations);
 
 
 /**
  * Q2.
  * It doesn't go as far as Dundee either, so remove it from the start of the array.
  */
-
-// console.log(stations);
+stations.shift();
+console.log(stations);
 
 
 
@@ -27,9 +28,8 @@ stations = ["Dundee", "Edinburgh", "North Berwick", "Newcastle", "York", "Leeds"
  * How many stations are on the line?
  */
 
-stationCount = null;
-
-// console.log("There are " + stationCount + " stations.");
+let stationCount = stations.length;
+console.log("There are " + stationCount + " stations.");
 
 
 
@@ -38,9 +38,8 @@ stationCount = null;
  * Which station is the third stop?
  */
 
-thirdStation = null;
-
-// console.log("The third station is " + thirdStation);
+thirdStation = stations[2];
+console.log("The third station is " + thirdStation);
 
 
 
@@ -48,8 +47,8 @@ thirdStation = null;
  * Q5.
  * Someone put the wrong Berwick on the timetable! Update "North Berwick" to "Berwick-upon-Tweed".
  */
-
-// console.log(stations);
+stations[2] = "Berwick-upon-Tweed";
+console.log(stations);
 
 
 
@@ -57,8 +56,8 @@ thirdStation = null;
  * Q6.
  * How did Leeds get on there? Better remove it.
  */
-
-// console.log(stations);
+stations.splice(stations.indexOf("Leeds"), 1);
+console.log(stations);
 
 
 
@@ -66,8 +65,8 @@ thirdStation = null;
  * Q7.
  * We forgot about Darlington! Add it to the route between Newcastle and York.
  */
-
-// console.log(stations);
+stations.splice(3, 0, "Darlington");
+console.log(stations);
 
 
 
@@ -76,12 +75,16 @@ thirdStation = null;
  * Is Durham one of the stops?
  */
 
-stoppingAtDurham = null;
-
-// console.log(stoppingAtDurham);
+stoppingAtDurham = stations.includes("Durham");
+console.log(stoppingAtDurham);
 
 
 /**
  * Q9.
  * If we are stopping at Durham, print "Yay Durham!" to the terminal. If not print "Aww...".
  */
+if (stoppingAtDurham) {
+    console.log("Yay Durham!");
+  } else {
+    console.log("Aww...");
+  }
